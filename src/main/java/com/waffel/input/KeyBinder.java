851d3387606ec670122df6f09876cd4d0c6e414c
Yoseph Sharif Alabdulwahab <Yosephsa0@gmail.com>
2016-08-03 07:44:10 +0300

    Documented Keyboard and KeyBinder

--- src/main/java/com/waffel/input/KeyBinder.java
+++ src/main/java/com/waffel/input/KeyBinder.java
@@ -10,14 +10,18 @@ import static com.waffel.input.KeyBinder.Binding.*;
  */
 public class KeyBinder {
 
+    // The enum for the key binding names available.
     public enum Binding {
         MV_FORWARD, MV_BACKWARD, MV_RIGHT, MV_LEFT, MV_BOOST;
     }
 
+    // The instance of this class.
     private static final KeyBinder instance = new KeyBinder();
 
+    //The hashmap to store all the key bindings with their corresponding keys.
     public HashMap<Binding, int[]> bindings = new HashMap<Binding, int[]>();
 
+    // Initializes the key bindings to the default bindings.
     private KeyBinder() {
         addBinding(MV_FORWARD, KeyEvent.VK_W);
         addBinding(MV_BACKWARD, KeyEvent.VK_S);
@@ -27,6 +31,11 @@ public class KeyBinder {
 
     }
 
+    /**
+     * Returns weather a certain binding is active. If none is available false is returned.
+     * @param a Binding Name/Identifier
+     * @return If that binding is pressed.
+     */
     public boolean isBindingActive(Binding a) {
         if(!bindings.containsKey(a))
             return false;
@@ -37,12 +46,26 @@ public class KeyBinder {
         return true;
     }
 
+    /**
+     * Will set the given binding name with the given keys. If the binding name is already exists it will be overrided.
+     * Will return false if another binding contains the same keys.
+     * @param a The binding Name/Identifier.
+     * @param keys The binding keys.
+     * @return True if no bindings with the same keys exists. False if so, or if keys is empty.
+     */
     public boolean addBinding(Binding a , int ... keys) {
         if(bindings.containsValue(keys))
             return false;
         return addBindingOverride(a, keys);
     }
 
+    /**
+     * Will set the given command to the given keys regardless if there already is another command with the same key
+     * bindings. If the binding name already exists it will be overrided.
+     * @param a The binding Name/Identifier.
+     * @param keys The binding keys.
+     * @return False if keys is empty.
+     */
     public boolean addBindingOverride(Binding a , int ... keys) {
         if(keys.length <= 0)
             return false;
@@ -50,12 +73,21 @@ public class KeyBinder {
         return true;
     }
 
+    /**
+     * Retrieves the keys for a given binding name. If binding name is not registered with any keys it will return null.
+     * @param a The binding name.
+     * @return An array of the ascii values of the keys that belong to this binding.
+     */
     public int[] getBinding(Binding a) {
         if(!bindings.containsKey(a))
-            return new int[0];
+            return null;
         return bindings.get(a);
     }
 
+    /**
+     * A accessor method for the singelton object of this class.
+     * @return The object of this class.
+     */
     public static KeyBinder getInstance() {
         return instance;
     }
--- src/main/java/com/waffel/input/Keyboard.java
+++ src/main/java/com/waffel/input/Keyboard.java
@@ -13,23 +13,41 @@ import static com.waffel.input.Keyboard.EventType.*;
  * Created by yosephsa on 8/2/16.
  */
 public class Keyboard implements KeyListener {
+    // The event types for when keys are changed in state.
     enum EventType {
         PRESSED, RELEASED, TYPED;
     }
 
+    // The instance of this singleton class' object.
     private static final Keyboard board = new Keyboard();
 
+    /* The key pressed stack that gets updated and cleared every game iteration. To keep track of all the keys that were
+     * pressed in between game iterations.
+     */
     private volatile Stack<EventData> keyStack = new Stack<>();
+    //The array of the state of all the keys.
     private boolean[] pressed = new boolean[255];
+    // The array of the keys that were pressed in between this iteration and the past.
     private boolean[] justPressed = new boolean[255];
+    // The array of the keys that were released in between this iteration and the past.
     private boolean[] justReleased = new boolean[255];
+    // The LinkedList of the character of the keys that were typed in between this iteration and the past.
     private LinkedList<Character> justTyped = new LinkedList<>();
 
 
     private Keyboard() {
-
     }
 
+    /**
+     * Updates the arrays that keep track of the keys' states.
+     * Clears the last modified lists (justPressed, justReleased ...) and pops elements from the keyStack and updates
+     * the other lists accordingly.
+     *
+     * The justPressed / released / typed lists are only set to true for a given key if that key was not in the state
+     * it is in this iteration.
+     *
+     * After updating the just... arrays the main state monitoring array (pressed) is updated.
+     */
     public void update() {
         justPressed = new boolean[255];
         justReleased = new boolean[255];
@@ -49,18 +67,37 @@ public class Keyboard implements KeyListener {
     }
 
 
-    public LinkedList getTypedCharacters() {
+    /**
+     * The keys stored in the linked list are affected by capslock and other os key modifications.
+     * @return The linked list storing the characters of all the keys that were just typed.
+     */
+    public LinkedList<Character> getTypedCharacters() {
         return justTyped;
     }
 
+    /**
+     * Will try to return the state of a given ascii value. If the value is not in range it will return false.
+     * @param ascii The ascii value of the desired key.
+     * @return Weather a given key is pressed.
+     */
     public boolean isPressed(int ascii) {
         return ascii >= 0 && ascii < pressed.length && pressed[ascii];
     }
 
+    /**
+     * Will try to return the state of a given ascii value. If the value is not in range it will return false.
+     * @param ascii The ascii value of the desired key.
+     * @return Weather a given was last pressed between now and the last game iteration.
+     */
     public boolean hasJustPressed(int ascii) {
         return ascii >= 0 && ascii < justPressed.length && justPressed[ascii];
     }
 
+    /**
+     * Will try to return the state of a given ascii value. If the value is not in range it will return false.
+     * @param ascii The ascii value of the desired key.
+     * @return Weather a given was last released between now and the last game iteration.
+     */
     public boolean hasJustReleased(int ascii) {
         return ascii >= 0 && ascii < justReleased.length && justReleased[ascii];
     }
@@ -82,10 +119,15 @@ public class Keyboard implements KeyListener {
             keyStack.push(new EventData(keyEvent, RELEASED));
     }
 
+    // Returns the instance of this singleton class.
     public static Keyboard getInstence() {
         return board;
     }
 
+    /**
+     * Stores the key event and the event type (pressed, released, etc.)
+     * Provides accessor methods for that information.
+     */
     private class EventData {
         private KeyEvent event;
         private EventType type;
